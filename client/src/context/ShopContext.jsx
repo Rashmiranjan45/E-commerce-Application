b3d9fb9a867e458cca39444e868df5767d570f84
rashmiranjan <samal.rashmiranjan45@gmail.com>
2026-01-19 20:19:14 +0530

    Add CartTotal component, enhance Cart and PlaceOrder pages; integrate Toast notifications and update Navbar for cart count

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext()
 
@@ -9,11 +11,15 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10
     const [search, setSearch] = useState("")
     const [showSearch, setShowSearch] = useState(false)
-    const [cartItem, setCartItem] = useState({})
-
+    const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
-        let cartData = structuredClone(cartItem)
+        if (!size) {
+            toast.error("Select Product Size")
+            return
+        }
+        let cartData = structuredClone(cartItems)
         if (cartData[itemId]) {
             if (cartData[itemId][size]) {
                 cartData[itemId][size] += 1
@@ -24,16 +30,65 @@ const ShopContextProvider = (props) => {
             cartData[itemId] = {}
             cartData[itemId][size] = 1
         }
-        setCartItem(cartData)
+        setCartItems(cartData)
     }
 
-    useEffect(() => {
-        console.log(cartItem)
-    }, [cartItem])
+    const getCartCount = () => {
+        let totalCount = 0
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item]
+                    }
+                } catch (error) {
+                    console.log("error", error)
+                }
+            }
+        }
+        return totalCount
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+        let cartData = structuredClone(cartItems)
+        cartData[itemId][size] = quantity
+
+        setCartItems(cartData)
+    }
+
+    const getCartAmount =  () => {
+        let totalAmount = 0;
+        for (const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items)
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount
+    }
+
+
 
 
     const value = {
-        products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItem, addToCart
+        products,
+        currency,
+        delivery_fee,
+        search, setSearch,
+        showSearch,
+        setShowSearch,
+        cartItems,
+        addToCart,
+        getCartCount,
+        updateQuantity,
+        getCartAmount,
+        navigate
     }
     return (
         <ShopContext.Provider value={value}>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { assets } from "../assets/frontend_assets/assets"
+import Title from "../components/Title"
+import NewsletterBox from "../components/NewsletterBox"
 
 const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <div className="text-2xl text-center pt-8 border-t">
+                <Title text1={"ABOUT"} text2={"US"} />
+            </div>
+            <div className="my-10 flex flex-col md:flex-row gap-16">
+                <img className="w-full md:max-w-112.5" src={assets.about_img} alt="" />
+                <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, voluptatem est accusantium perferendis delectus exercitationem error excepturi fugiat nemo nulla corrupti pariatur animi, corporis, ipsa vitae amet neque. Itaque, temporibus?</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nisi excepturi veniam. Odio, fugiat. Similique suscipit dolor distinctio id aut itaque ipsum laboriosam numquam fuga.</p>
+                    <b className="text-gray-800">Our Mission</b>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi qui nulla expedita consequuntur aliquid, voluptatum magni fuga vero dolor, ipsum maxime tempora ullam iusto voluptates culpa ut laudantium nobis. Temporibus?</p>
+
+                </div>
+            </div>
+            <div className="text-xl py-4">
+                <Title text1={"WHY"} text2={"CHOOSE US"}/>
+            </div>
+            <div className="flex flex-col md:flex-row text-sm mb-20">
+                <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+                    <b>Quality Assurance:</b>
+                    <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum labore asperiores libero. Perspiciatis voluptatum dolorem dignissimos, repellendus iste minus ipsa rem reiciendis dolores, libero quia eligendi deserunt ducimus, expedita corporis!</p>
+                </div>
+                <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+                    <b>Convenience:</b>
+                    <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum labore asperiores libero. Perspiciatis voluptatum dolorem dignissimos, repellendus iste minus ipsa rem reiciendis dolores, libero quia eligendi deserunt ducimus, expedita corporis!</p>
+                </div>
+                <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+                    <b>Exceptional Customer Service:</b>
+                    <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum labore asperiores libero. Perspiciatis voluptatum dolorem dignissimos, repellendus iste minus ipsa rem reiciendis dolores, libero quia eligendi deserunt ducimus, expedita corporis!</p>
+                </div>
+            </div>
+            <NewsletterBox/>
+        </div>
     )
 }
 
